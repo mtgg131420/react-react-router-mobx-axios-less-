361332bf8b9bece0d6efc59d9b85355ae672ed6b
mtgg131420 <L13711932052@outlook.com>
2026-01-18 16:14:46 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import aboutStore from './about';
-import homeStore from './home';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = {
-	aboutStore,
-	homeStore
-};
+// 引入移动的适配器（根据屏幕宽度来设置html的sont-size基准值）
+import  'utils/adapter';
 
-export default store;
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
